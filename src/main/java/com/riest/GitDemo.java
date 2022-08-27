2c9500dfcf1e5bf91e98db00f394820243a83c59
dgj <2575101192@qq.com>
2022-08-27 14:50:39 +0800

    dgj-master-first-commit

--- src/main/java/com/riest/GitDemo.java
+++ src/main/java/com/riest/GitDemo.java
@@ -4,6 +4,8 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("Hello 1");
         System.out.println("Hello 2");
+
         System.out.println("Hello 3");
+        System.out.println("hello 4");
     }
 }
