39c6069d01f3aa127032ddc225f174f23db66732
dgj <2575101192@qq.com>
2022-08-27 14:48:11 +0800

    master-third-commit

--- src/main/java/com/riest/GitDemo.java
+++ src/main/java/com/riest/GitDemo.java
@@ -4,5 +4,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("Hello 1");
         System.out.println("Hello 2");
+        System.out.println("Hello 3");
     }
 }
